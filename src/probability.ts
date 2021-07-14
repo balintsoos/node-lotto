03f9a3562676f8a064f76232755f48d1552078d0
Balint Soos <soosbalint95@gmail.com>
2021-07-14 19:57:03 +0200

    use easier to understand algo for combinations

--- src/probability.ts
+++ src/probability.ts
@@ -1,6 +1,6 @@
 export const combinations = (numbers: number[], k: number): number[][] => {
   const result: number[][] = [];
-  recurseCombinations(numbers, k, result, new Array(k), 0, numbers.length - 1, 0);
+  recurseCombinations(numbers, k, result, new Array(k), 0, 0);
   return result;
 };
 
@@ -9,18 +9,19 @@ const recurseCombinations = (
   k: number,
   result: number[][],
   temp: number[],
-  start: number,
-  end: number,
-  index: number,
-): void => {
-  if (index === k) {
+  tempIndex: number,
+  numbersIndex: number,
+) => {
+  if (tempIndex === k) {
     result.push([...temp]);
     return;
   }
-  for (let i = start; i <= end && end - i + 1 >= k - index; i++) {
-    temp[index] = numbers[i];
-    recurseCombinations(numbers, k, result, temp, i + 1, end, index + 1);
+  if (numbersIndex === numbers.length) {
+    return;
   }
+  temp[tempIndex] = numbers[numbersIndex];
+  recurseCombinations(numbers, k, result, temp, tempIndex + 1, numbersIndex + 1);
+  recurseCombinations(numbers, k, result, temp, tempIndex, numbersIndex + 1);
 };
 
 export const combination = (n: number, k: number): number => factorial(n) / (factorial(k) * factorial(n - k));
